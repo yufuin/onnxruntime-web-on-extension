02c8974f65de395cdd9f57f97502cd1c6fc6c466
tsujimura.12052 <sd18602@tti-j.net>
2023-12-22 06:41:32 +0900

    change quote

--- ext/src/service_worker.ts
+++ ext/src/service_worker.ts
@@ -1,12 +1,12 @@
-import * as ort from 'onnxruntime-web';
+import * as ort from "onnxruntime-web";
 ort.env.wasm.numThreads = 1; // ref: https://github.com/microsoft/onnxruntime/issues/14445
-// ort.env.wasm.wasmPaths = './';
+ort.env.wasm.wasmPaths = "./";
 
 async function test_ort() {
     try {
         // setup session
-        const session = await ort.InferenceSession.create('./model.onnx', {executionProviders: ['wasm']});
-        console.log('session:', session);
+        const session = await ort.InferenceSession.create("./model.onnx", {executionProviders: ["wasm"]});
+        console.log("session:", session);
         console.log(`input names: ${session.inputNames}`);
         console.log(`output names: ${session.outputNames}`);
 
@@ -14,7 +14,7 @@ async function test_ort() {
         const batch_size = 1;
         const input_dim = 2;
         const input_tensor_data = new Float32Array([2.5, 4.25]); // the data buffer is a flattened tensor (shape=[1,2] => num_elems=[1*2]=[2]).
-        const input_tensor = new ort.Tensor('float32', input_tensor_data, [batch_size, input_dim]);
+        const input_tensor = new ort.Tensor("float32", input_tensor_data, [batch_size, input_dim]);
         console.log(`flattened input tensor: [${input_tensor.data}] (original shape=[${input_tensor.dims}])`);
 
         // forward
@@ -24,10 +24,10 @@ async function test_ort() {
         const output_tensor = results.y;
         console.log(`flattened output tensor: [${output_tensor.data}] (original shape=[${output_tensor.dims}])`);
 
-        console.log('session successfully run.');
+        console.log("session successfully run.");
 
     } catch (error) {
-        console.log('error:', error);
+        console.log("error:", error);
     }
 }
 test_ort();
